0e4174165baa9e092eff06293e9d5611b5d2ac97
WojciechZankowski <info@zankowski.pl>
2016-04-17 00:54:38 +0200

    Unit testing

--- blazar-fix-parser/src/main/java/com/blazarquant/bfp/core/security/util/SecurityUtilImpl.java
+++ blazar-fix-parser/src/main/java/com/blazarquant/bfp/core/security/util/SecurityUtilImpl.java
@@ -26,6 +26,9 @@ public class SecurityUtilImpl implements SecurityUtil {
 
     @Override
     public String generateConfirmationKey(long id, String username, String email) {
+        if (username.isEmpty() || email.isEmpty()) {
+            throw new IllegalArgumentException("Failed to generate confirmation key. Username or email is empty.");
+        }
         return Base64.encodeToString(
                 buildKeyBase(id, username, email).getBytes());
     }
@@ -37,7 +40,7 @@ public class SecurityUtilImpl implements SecurityUtil {
     @Override
     public long decodeConfirmationKey(String confirmationKey) throws DecodingException {
         String[] elements = Base64.decodeToString(confirmationKey).split(KEY_DELIMITER);
-        if (elements.length == 0) {
+        if (elements.length != 3) {
             throw new IllegalArgumentException("Illegal confirmation key. Please contact with admin.");
         }
         return Long.parseLong(elements[0]);
