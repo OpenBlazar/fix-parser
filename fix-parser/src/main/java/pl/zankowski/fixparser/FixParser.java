9835040051a2fdcd8ce25e92849ab1609245ecfd
WojciechZankowski <info@zankowski.pl>
2018-12-08 20:58:37 +0100

    Refactor

--- fix-parser/src/main/java/pl/zankowski/fixparser/FixParser.java
+++ fix-parser/src/main/java/pl/zankowski/fixparser/FixParser.java
@@ -3,14 +3,6 @@ package pl.zankowski.fixparser;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
-import org.apache.shiro.guice.aop.ShiroAopModule;
-import pl.zankowski.fixparser.mail.MailModule;
-import pl.zankowski.fixparser.messages.MessageModule;
-import pl.zankowski.fixparser.messages.share.ShareModule;
-import pl.zankowski.fixparser.payment.PaymentModule;
-import pl.zankowski.fixparser.tracker.TrackerModule;
-import pl.zankowski.fixparser.user.UserModule;
-import pl.zankowski.fixparser.web.WebModule;
 import pl.zankowski.fixparser.web.security.SecurityModule;
 
 import javax.servlet.ServletContext;
@@ -29,15 +21,8 @@ public class FixParser extends GuiceServletContextListener {
     @Override
     protected Injector getInjector() {
         return Guice.createInjector(
-                new MailModule(),
-                new MessageModule(),
-                new ShareModule(),
-                new PaymentModule(),
-                new TrackerModule(),
-                new UserModule(),
-                new SecurityModule(servletContext),
-                new ShiroAopModule(),
-                new WebModule());
+                new FixParserModule(),
+                new SecurityModule(servletContext));
     }
 
 }
